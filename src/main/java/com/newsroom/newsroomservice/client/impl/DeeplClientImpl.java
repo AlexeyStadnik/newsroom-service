42ef963a16fc7670c60ff59ca87b07d70791d200
aliaksei.stadnik <stadnik911@gmail.com>
2023-04-23 20:20:38 +0100

    Add handling of Deepl quota exceeded exception

--- src/main/java/com/newsroom/newsroomservice/client/impl/DeeplClientImpl.java
+++ src/main/java/com/newsroom/newsroomservice/client/impl/DeeplClientImpl.java
@@ -1,5 +1,6 @@
 package com.newsroom.newsroomservice.client.impl;
 
+import com.deepl.api.QuotaExceededException;
 import com.deepl.api.TextResult;
 import com.deepl.api.Translator;
 import com.newsroom.newsroomservice.client.DeeplClient;
@@ -7,6 +8,7 @@ import com.newsroom.newsroomservice.exception.ServiceException;
 import com.service.model.rest.RestLanguage;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeeplClientImpl implements DeeplClient {
@@ -31,25 +34,25 @@ public class DeeplClientImpl implements DeeplClient {
             return new ArrayList<>();
         }
 
-        final List<TextResult> textResults = deeplTranslator.translateText(
-                toTranslate,
-                "EN",
-                toTargetLanguage(target)
-        );
+        try {
+            final List<TextResult> textResults
+                    = deeplTranslator.translateText(toTranslate, "EN", toTargetLanguage(target)
+            );
 
-        return textResults.stream().map(TextResult::getText).collect(Collectors.toList());
+            return textResults.stream().map(TextResult::getText).collect(Collectors.toList());
+        } catch (QuotaExceededException e) {
+            throw new ServiceException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "DeepL Quota exceeded, for now we are not able to translate", e);
+        }
     }
 
 
     private static String toTargetLanguage(final RestLanguage sourceLanguage) {
-        switch (sourceLanguage) {
-            case EN:
-                return "EN-US";
-            case PT:
-                return "PT-PT";
-            default:
-                throw new ServiceException(HttpStatus.BAD_REQUEST,
-                        String.format("Unsupported language: %s", sourceLanguage));
-        }
+        return switch (sourceLanguage) {
+            case EN -> "EN-US";
+            case PT -> "PT-PT";
+            default -> throw new ServiceException(HttpStatus.BAD_REQUEST,
+                    String.format("Unsupported language: %s", sourceLanguage));
+        };
     }
 }

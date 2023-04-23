67220858fb810cca5c37753d59ee43ca15c33e98
aliaksei.stadnik <stadnik911@gmail.com>
2023-04-23 14:45:12 +0100

    Prepare initial project skeleton
    Includes: openapi generator, static analysis configuration, spring db and flyway configuration

---
+++ src/test/java/com/newsroom/newsroomservice/NewsroomServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.newsroom.newsroomservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsroomServiceApplicationTests {
+
+    public void contextLoads() {
+    }
+
+}
